24dcb8e862de7b321db609ecd143ceab5f1c0350
Immortal-Dream <tonyliu02@outlook.com>
2025-02-19 21:58:00 +0000

    fix: comm local and node.js

--- distribution/local/comm.js
+++ distribution/local/comm.js
@@ -1,104 +1,81 @@
-/** @typedef {import("../types").Callback} Callback */
-/** @typedef {import("../types").Node} Node */
-const util = require('../util/util');
-
-/**
- * @typedef {Object} Target
- * @property {string} service
- * @property {string} method
- * @property {Node} node
- */
 const http = require("http");
+const util = global.distribution.util;
 
-
-/**
- * Sends a message to a remote service via HTTP PUT request.
- * @param {Array} message
- * @param {Target} remote
- * @param {Callback} [callback]
- * @return {void}
- */
-function send(message, remote, callback) {
-    
-    const hasCallback = typeof callback === 'function';
-    let jsonMessage;
-
-    try {
-        // DEBUG
-        // console.log("1 Message: "+message);
-        jsonMessage = util.serialize(message);
-        // console.log("2 jsonMessage: "+jsonMessage);
-        // console.log("3 Util Json Message: "+ util.serialize(message));
-        
-    } catch (e) {
-        if (hasCallback) {
-            callback(e);
-        }
-        return;
-    }
-    if (!remote || remote.node === undefined || remote.method === undefined || remote.service === undefined) {
-        if (hasCallback) {
-            callback(new Error('Invalid remote configuration'));
-        }
-        return;
+function send(message, remote, callback = () => {}) {
+  try {
+    if (!remote || !remote.node || !remote.service || !remote.method) {
+      callback(new Error("Invalid remote target information"));
+      return;
     }
 
-    const gid = remote.gid || "local"; 
+    if (!global.nodeConfig) {
+      callback(new Error("Global node configuration is missing"));
+      return;
+    }
+
+    // Ensure proper gid handling
+    const gid = remote.gid || "local";  
     const path = `/${gid}/${remote.service}/${remote.method}`;
+
+    // Prepare request payload
+    const payload = util.serialize({ args: message });
+
     const options = {
-        host: remote.node.ip,
-        port: remote.node.port,
-        path: path,
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(jsonMessage)
-        }
+      hostname: remote.node.ip,
+      port: remote.node.port,
+      path: path,
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.byteLength(payload),
+      },
+      timeout: 5000, // Added timeout
     };
 
+    console.error(payload);
+    console.error(options);
+
     const req = http.request(options, (res) => {
-        if (!hasCallback) {
-            res.resume(); // Drain the response if no callback
+      let responseData = "";
+
+      res.on("data", (chunk) => {
+        responseData += chunk;
+      });
+
+      res.on("end", () => {
+        if (res.statusCode === 404) {
+          callback(new Error(`Service "${remote.service}" not found in gid "${gid}"`));
+          return;
+        }
+
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          callback(new Error(`HTTP error: ${res.statusCode} - ${responseData}`));
+          return;
+        }
+
+        try {
+          const result = util.deserialize(responseData);
+          if (result.error) {
+            callback(new Error(result.error));
             return;
+          }
+          callback(null, result.value);
+          return;
+        } catch (err) {
+          callback(new Error("Failed to parse server response"));
+          return;
         }
-
-        let chunks = [];
-        res.on('data', (chunk) => chunks.push(chunk));
-        res.on('end', () => {
-            const body = chunks.length > 0 ? Buffer.concat(chunks).toString() : '';
-            // console.log("Body"+body);
-            if (res.statusCode === 200) {
-                try {
-                    const result = util.deserialize(body);     
-                    // console.log("result: " + result); DEBUG
-                    callback(null, result);
-                } catch (e) {
-                    console.log(e);
-                    callback(new Error('Failed to parse JSON response'));
-                }
-            } else {
-                let errMsg;
-                try {
-                    const errorBody = util.deserialize(body);
-                    errMsg = errorBody.error || body;
-                } catch (e) {
-                    errMsg = body;
-                }
-                const error = new Error(`Request failed with status code ${res.statusCode}: ${errMsg}`);
-                error.statusCode = res.statusCode;
-                callback(error);
-            }
-        });
+      });
     });
 
-    req.on('error', (err) => {
-        if (hasCallback) {
-            callback(err);
-        }
-    });
+    req.on("error", (err) => callback(new Error(`Request error: ${err.message}`)));
 
-    req.write(jsonMessage);
+    req.write(payload);
     req.end();
+  } catch (error) {
+    callback(new Error("Unexpected error in comm.send: " + error.message));
+    return;
+  }
 }
 
 module.exports = { send };
--- distribution/local/node.js
+++ distribution/local/node.js
@@ -1,130 +1,91 @@
 const http = require('http');
 const url = require('url');
 const log = require('../util/log');
-const util = require('../util/util');
+const util = global.distribution.util;
+const routes = global.distribution.local.routes;
 
-const routes = require('./routes');
-/*
-    The start function will be called to start your node.
-    It will take a callback as an argument.
-    After your node has booted, you should call the callback.
-*/
-
-
-const start = function (callback) {
+/**
+ * Starts the HTTP server to handle incoming requests.
+ * The server listens for PUT requests and routes them to the appropriate service.
+ * @param {Function} callback - Callback function executed once the server starts.
+ */
+const start = function(callback) {
   const server = http.createServer((req, res) => {
-    /* Your server will be listening for PUT requests. */
-
-    // Write some code...
+    // Only allow PUT requests
     if (req.method !== 'PUT') {
       res.writeHead(405, { 'Content-Type': 'text/plain' });
       res.end('Method Not Allowed');
       return;
     }
 
-    /*
-      The path of the http request will determine the service to be used.
-      The url will have the form: http://node_ip:node_port/service/method
-    */
+    // Parse URL to extract gid, service, and method
+    const parsedUrl = url.parse(req.url, true);
+    const pathSegments = parsedUrl.pathname.split('/').filter(Boolean);
 
-    // Write some code...
-    const parsedUrl = url.parse(req.url);
-    const pathParts = parsedUrl.pathname.split('/').filter(part => part !== '');
-    
-    let gid, serviceName, methodName;
-    if (pathParts.length === 3) {
-      // Path has the form /<gid>/<service>/<method>
-      [gid, serviceName, methodName] = pathParts;
-    } else if (pathParts.length === 2) {
-      // Path has the form /<service>/<method>; default gid to "local"
-      gid = 'local';
-      [serviceName, methodName] = pathParts;
-    } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not Found');
+    if (pathSegments.length < 3) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(util.serialize({ error: "Invalid request format" }));
       return;
     }
 
-    /*
+    const [gid, service, method] = pathSegments;
 
-      A common pattern in handling HTTP requests in Node.js is to have a
-      subroutine that collects all the data chunks belonging to the same
-      request. These chunks are aggregated into a body variable.
-
-      When the req.on('end') event is emitted, it signifies that all data from
-      the request has been received. Typically, this data is in the form of a
-      string. To work with this data in a structured format, it is often parsed
-      into a JSON object using JSON.parse(body), provided the data is in JSON
-      format.
-
-      Our nodes expect data in JSON format.
-  */
-
-    // Write some code...
-    let body = [];
-
-    req.on('data', (chunk) => {
-      // Collect each chunk of data.
-      body.push(chunk);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
     });
 
-    req.on('end', () => {
-      let args;
+    req.on("end", () => {
       try {
-        const rawBody = Buffer.concat(body).toString();
-        args = util.deserialize(rawBody);
-        if (!Array.isArray(args)) {
-          throw new Error('Expected an array of arguments');
-        }
-      } catch (e) {
-        res.writeHead(400, { 'Content-Type': 'application/json' });
-        res.end(util.serialize({ error: e.message }));
-        return;
-      }
+        // Parse request body
+        const parsedBody = util.deserialize(body);
+        const args = parsedBody.args || [];
 
-      // Use routes.get with a configuration object so that the group is respected.
-      routes.get({ service: serviceName, gid: gid }, (err, service) => {
-        if (err) {
-          res.writeHead(404, { 'Content-Type': 'application/json' });
-          res.end(util.serialize({ error: err.message }));
-          return;
-        }
-  
-        const method = service[methodName];
-        if (typeof method !== 'function') {
-          res.writeHead(404, { 'Content-Type': 'application/json' });
-          res.end(util.serialize({ error: `Method ${methodName} not found in service ${serviceName}` }));
-          return;
-        }
-  
-        try {
-          method(...args, (error, value) => {
+        // Retrieve the requested service based on gid and service name
+        routes.get({ service, gid }, (err, serviceObj) => {
+          if (err) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(util.serialize({ error: `Service "${service}" not found in gid "${gid}"` }));
+            return;
+          }
+
+          // Check if the requested method exists in the service
+          if (typeof serviceObj[method] !== "function") {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(util.serialize({ error: `Method "${method}" not found in service "${service}"` }));
+            return;
+          }
+
+          // Execute the requested method and handle response
+          serviceObj[method](...args, (error, result) => {
             if (error) {
               res.writeHead(500, { 'Content-Type': 'application/json' });
               res.end(util.serialize({ error: error.message }));
-            } else {
-              res.writeHead(200, { 'Content-Type': 'application/json' });
-              res.end(util.serialize(value));
+              return;
             }
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(util.serialize({ value: result }));
           });
-        } catch (e) {
-          res.writeHead(500, { 'Content-Type': 'application/json' });
-          res.end(util.serialize({ error: `Internal server error: ${e.message}` }));
-        }
-      });
+        });
+
+      } catch (err) {
+        // Handle JSON parsing errors
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(util.serialize({ error: "Invalid JSON payload" }));
+        return;
+      }
     });
   });
 
-  /*
-    Your server will be listening on the port and ip specified in the config.
-    When the server has successfully started, call the provided callback.
-  */
+  // Start the server on the specified IP and port
   server.listen(global.nodeConfig.port, global.nodeConfig.ip, () => {
     log(`Server running at http://${global.nodeConfig.ip}:${global.nodeConfig.port}/`);
     global.distribution.node.server = server;
     callback(server);
+    return;
   });
 
+  // Handle server errors
   server.on('error', (error) => {
     log(`Server error: ${error}`);
     throw error;
@@ -132,5 +93,5 @@ const start = function (callback) {
 };
 
 module.exports = {
-  start: start,
+  start,
 };
