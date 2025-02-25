7de1cb8bcf6f6d39b43d93d687a519cc532231dd
Immortal-Dream <tonyliu02@outlook.com>
2025-02-25 14:51:39 +0000

    feature: finish the local mem

--- distribution/local/mem.js
+++ distribution/local/mem.js
@@ -1,11 +1,59 @@
+const crypto = require('crypto');
 
-function put(state, configuration, callback) {
-};
+// In-memory store for key-value pairs
+const store = {};
 
-function get(configuration, callback) {
+/**
+ * Stores a value under a given key.
+ * If key is null, computes the key as the sha256 hash of the JSON serialized value.
+ *
+ * @param {*} value - The object to be stored.
+ * @param {string|null} key - The primary key; if null, a hash is computed.
+ * @param {function(Error, *): void} callback - The callback to invoke with (error, value).
+ */
+function put(value, key, callback) {
+  // If no key is provided, compute the sha256 hash of the serialized object
+  if (key === null) {
+    key = crypto.createHash('sha256').update(JSON.stringify(value)).digest('hex');
+  }
+  // Idempotently store or update the value under the key
+  store[key] = value;
+  callback(null, value);
 }
 
-function del(configuration, callback) {
-};
+/**
+ * Retrieves a value from the store using the key.
+ *
+ * @param {string} key - The primary key.
+ * @param {function(Error, *): void} callback - The callback to invoke with (error, value).
+ */
+function get(key, callback) {
+    if (key === null) {
+      // If no key is provided, return an array of all keys in the store.
+      callback(null, Object.keys(store));
+    } else {
+      if (Object.prototype.hasOwnProperty.call(store, key)) {
+        callback(null, store[key]);
+      } else {
+        callback(new Error("Key not found"), null);
+      }
+    }
+  }
 
-module.exports = {put, get, del};
+/**
+ * Deletes a value from the store using the key.
+ *
+ * @param {string} key - The primary key.
+ * @param {function(Error, *): void} callback - The callback to invoke with (error, value).
+ */
+function del(key, callback) {
+  if (Object.prototype.hasOwnProperty.call(store, key)) {
+    const value = store[key];
+    delete store[key];
+    callback(null, value);
+  } else {
+    callback(new Error("Key not found"), null);
+  }
+}
+
+module.exports = { put, get, del };
